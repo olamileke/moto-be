a98c233e58147ae216c563aa4d131b521b523006
Fambegbe Olamileke <olamileke.f@gmail.com>
2020-07-22 20:59:48 +0100

    removed config.js from .gitignore

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../resources/auth.js');
-const { body } = require('express-validator');
-
-router.post('/authenticate', [ body('email').isEmail(), body('password').isLength({ min:8 })]  , auth.post);
-
-module.exports = router; 
--- routes/passwordresets.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const passordresets = require('../resources/passwordresets');
-
-router.post('/passwords/reset', passordresets.post);
-
-module.exports = router;
